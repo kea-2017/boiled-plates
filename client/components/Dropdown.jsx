8380ab0bc1e55aea76e42fa724836e1bd6849a06
gabriel <777canaan@gmail.com>
2017-08-17 11:50:03 +1200

    map showing

--- client/components/Dropdown.jsx
+++ client/components/Dropdown.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
-import { ButtonToolbar, DropdownButton, MenuItem, Button, Pager } from 'react-bootstrap';
+import {
+  ButtonToolbar,
+  DropdownButton,
+  MenuItem,
+  Button,
+  Pager,
+  Grid,
+  Row,
+  Col,
+  Input
+} from 'react-bootstrap'
 
 export default class Dropdown extends React.Component {
   constructor(props) {
@@ -12,18 +22,70 @@ export default class Dropdown extends React.Component {
     }
   }
   render() {
-    const BUTTONS = ['Default', 'Primary', 'Success', 'Info', 'Warning', 'Danger']
+    const BUTTONS = [
+      'Default',
+      'Primary',
+      'Success',
+      'Info',
+      'Warning',
+      'Danger'
+    ]
     return (
       <div>
+        <Grid>
+          <Row className="header">
+            <Col xs={2} md={2}></Col>
+          <Col xs={10} md={10}>
+        <ButtonToolbar>{BUTTONS.map(renderDropdownButton)}
+        </ButtonToolbar></Col>
+        <Col xs={2} md={2}></Col>
         <Link to='/'>
-         <button type="button" className="btn btn-primary">Home</button>
-       </Link>
-       <ButtonToolbar>{BUTTONS.map(renderDropdownButton)}
-       </ButtonToolbar>
-       <Pager>
-         <Pager.Item previous href="/">&larr; Previous Page</Pager.Item>
-         <Pager.Item next href="#/map">Next Page &rarr;</Pager.Item>
-       </Pager>
+        <button type="button" className="btn btn-primary">Home</button>
+      </Link>
+      </Row>
+    </Grid>
+        <Row>
+          <br></br>
+          <br></br>
+          <br></br>
+        </Row>
+        <ButtonToolbar>
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={2} md={3}></Col>
+              <Col xs={4} md={3}>
+                <Button bsStyle="primary" bsSize="large">Prescriptions</Button>
+              </Col>
+              <Col xs={2} md={3}></Col>
+              <Col xs={2} md={3}></Col>
+              <Col xs={4} md={3}>
+                <Button bsStyle="primary" bsSize="large">Referals</Button>
+              </Col>
+              <Col xs={2} md={3}></Col>
+            </Row>
+          </Grid>
+          <Row>
+            <br></br>
+          </Row>
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={2} md={3}></Col>
+              <Col xs={4} md={3}>
+                <Button bsStyle="primary" bsSize="large">Advice</Button>
+              </Col>
+              <Col xs={2} md={3}></Col>
+              <Col xs={2} md={3}></Col>
+              <Col xs={4} md={3}>
+                <Button bsStyle="primary" bsSize="large">Warnings</Button>
+              </Col>
+              <Col xs={2} md={3}></Col>
+            </Row>
+          </Grid>
+        </ButtonToolbar>
+        <Pager>
+          <Pager.Item previous href="/">&larr; Previous Page</Pager.Item>
+          <Pager.Item next href="#/map">Next Page &rarr;</Pager.Item>
+        </Pager>
       </div>
     )
   }
@@ -35,7 +97,7 @@ function renderDropdownButton(title, i) {
       <MenuItem eventKey="1">Action</MenuItem>
       <MenuItem eventKey="2">Another action</MenuItem>
       <MenuItem eventKey="3" active>Active Item</MenuItem>
-      <MenuItem divider />
+      <MenuItem divider/>
       <MenuItem eventKey="4">Separated link</MenuItem>
     </DropdownButton>
   )
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import {ButtonToolbar,DropdownButton,MenuItem,Button,
+  Pager,Grid,Row,Col,Input} from 'react-bootstrap'
 
 const Header = () => {
   return (
     <div>
-      <div className="page-header">
-        <h1>Hello Dave<small>wanna buy some pegs</small>
-        </h1>
-      </div>
+      <Grid>
+        <Row className="header">
+          <Col xs={1} md={4}></Col>
+          <Col xs={4} md={4}>
+            <h1>Hello Dave<small>wanna buy some pegs</small>
+            </h1>
+          </Col>
+          <Col xs={1} md={4}></Col>
+        </Row>
+      </Grid>
     </div>
   )
 }
